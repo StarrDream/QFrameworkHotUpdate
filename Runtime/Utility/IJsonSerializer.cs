d3a878f11ac19d34df88fa3e50527296115429f2
StarryDream <98099121+StarrDream@users.noreply.github.com>
2025-09-01 04:18:20 +0800

    Delete Runtime directory

--- Runtime/Utility/IJsonSerializer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace QHotUpdateSystem.Utility
-{
-    public interface IJsonSerializer
-    {
-        string Serialize(object obj, bool pretty = false);
-        T Deserialize<T>(string json);
-    }
-}
